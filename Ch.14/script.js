6e3d4f710348d157f6f159670f64de7ee07b3806
Cesar Ojeda <cesar.mso@gmail.com>
2024-04-11 15:18:49 +0200

    completed upto exercise 80

--- Ch.14/script.js
+++ Ch.14/script.js
@@ -1,5 +1,31 @@
-/* Code sample - Section 14, excercise 76*/
+/* Code sample - Section 14, excercise 76
 window.alert("Hello");
 alert("World");
-console.log("done!");
+console.log("done!");*/
 
+/* Code sample - Section 14, excercise 77 
+var solution = 2 + 3;
+
+alert(typeof(solution));
+alert(solution);*/
+
+/* Code sample - Section 14, exercise 78 
+var myName = prompt("What's your name?");
+alert("Hello " + myName + ", nice to meet you!!");*/
+
+/* Code sample - Section 14, excercise 79+80 - coding exercise 
+
+    var a = "3";
+    var b = "8";
+
+
+    /* My code - START */
+    /*var c = a;
+    a = b;
+    b = c;*/
+    /* My code - END*/
+
+    /*console.log("a is " + a);
+    console.log("b is " + b);*/
+
+/* Code sample - Section 14, excercise 79 - coding exercise */
